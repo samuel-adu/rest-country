53f9b8ee71d55dfd1b0597d0bc6c1574825601e3
Samuel Adu <theadusamuel@gmail.com>
2024-06-13 01:46:20 +0100

    resize images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,27 +5,33 @@ function Card({ country }) {
   const { name, flags, population, region, capital, showCountryDetails } =
     country;
   return (
-    <Link to={`${name.common}`} onClick={showCountryDetails}>
-      <div className="card">
-        <img className="card-img" src={flags.svg} alt={`flag of ${name}`} />
-
-        <div className="card-desc">
-          <p className="card-heading">{name.common}</p>
-          <p className="card-text">
-            Population:
-            <span>{population.toLocaleString()}</span>
-          </p>
-          <p className="card-text">
-            Region:
-            <span>{region}</span>
-          </p>
-          <p className="card-text">
-            Capital:
-            <span>{capital}</span>
-          </p>
-        </div>
+    <div className="card">
+      <div className="card-image">
+        <Link
+          to={`${name.common}`}
+          onClick={showCountryDetails}
+          className="link-card"
+        >
+          <img className="card-img" src={flags.svg} alt={`flag of ${name}`} />
+        </Link>
       </div>
-    </Link>
+
+      <div className="card-desc">
+        <p className="card-heading">{name.common}</p>
+        <p className="card-text">
+          Population:
+          <span>{population.toLocaleString()}</span>
+        </p>
+        <p className="card-text">
+          Region:
+          <span>{region}</span>
+        </p>
+        <p className="card-text">
+          Capital:
+          <span>{capital}</span>
+        </p>
+      </div>
+    </div>
   );
 }
 
